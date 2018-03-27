0a6e8e260731969c99a3f6f97d7e040dc8a0aa3d
ervinbani <bani.ervin@gmail.com>
2018-03-27 14:54:50 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+/*Chiedi all'utente di inserire un numero massimo (chiamato max) e un numero
+minimo (chiamato min) con due prompt diversi.
+In seguito devi chiedere all'utente un numero positivo, che chiamiamo N.
+
+Il programma dovrà generare N array, ogni array sarà formato da 10 numeri interi
+ random contenuti nel range min e max.
+
+Una volta costruiti questi array, il programma dovrà generare una lista delle
+ occorrenze.Nello specifico, guardando tutti i numeri inclusi in tutti
+  gli array, dovrà indicare quante volte compare ogni numero in tutti gli array.
