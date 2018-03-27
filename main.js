1b8f09b11f8a6602a14031b121ddd5cf75abfb52
ervinbani <bani.ervin@gmail.com>
2018-03-27 15:58:15 +0200

    arr

--- main.js
+++ main.js
@@ -10,4 +10,32 @@ Una volta costruiti questi array, il programma dovrà generare una lista delle
   gli array, dovrà indicare quante volte compare ogni numero in tutti gli array.
 */
 
-var min=parseInt("inserire un numero : ");
+var min=parseInt(prompt("inserire un numero : "));
+var max=parseInt(prompt("inserire il un numero max: "));
+
+var N=parseInt(prompt("inserire il un numero intero: "));
+
+//creo un array iniziale vuoto
+var array=[];
+
+for(var i=0;i<N;i++){
+  array[i]=generaArray(min, max);
+
+}
+console.log(array);
+
+
+
+function generaArray(num1, num2){
+  var arr=[];
+  var i=0;
+  do{
+    if(num1<=(Math.floor(Math.random()*num2+1))){
+        arr.push(Math.floor(Math.random()*num2+1));
+        i++;
+    }
+  }while(i<10)
+  return arr;
+
+
+}
